a304ac6904c3206a1f5ce16002084a5d3a15d3a5
ilya07092000 <ischenko2012@gmail.com>
2023-10-16 19:21:33 +0300

    feat: error handler middleware

---
+++ src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,23 @@
+import HttpException from '@src/infrastructure/exceptions/httpException';
+import ValidationException from '@src/infrastructure/exceptions/validationException';
+
+const errorHandlerMiddleWare = (error, req, res, next) => {
+  if (
+    error &&
+    (error instanceof HttpException || error instanceof ValidationException)
+  ) {
+    return res.status(error.code || 400).json({
+      error: {
+        message: error.message || 'Something Went Wrong',
+      },
+    });
+  }
+
+  return res.status(500).json({
+    error: {
+      message: 'Something Went Wrong',
+    },
+  });
+};
+
+export default errorHandlerMiddleWare;
