3e4ceae884431c54415d33d4a73b7908036036c4
ilya07092000 <ischenko2012@gmail.com>
2023-10-17 15:48:14 +0300

    feat: token repository

---
+++ src/repositories/tokenRepository.ts
@@ -0,0 +1,22 @@
+import { redisConnectionInstance } from '@src/db/redis/connection';
+import IToken from '@src/infrastructure/interfaces/IToken';
+
+class TokenRepository {
+  private connection;
+
+  constructor(connection) {
+    this.connection = connection;
+  }
+
+  save({ token, userId }: { token: IToken; userId: number }) {
+    const { token: key, ...tokenInfo } = token;
+    return this.connection.hSet(key, { ...tokenInfo, userId });
+  }
+
+  get({ token }: { token: string }): Promise<IToken & { userId: number }> {
+    return this.connection.hGetAll(token);
+  }
+}
+
+const tokenRepository = new TokenRepository(redisConnectionInstance.connection);
+export { TokenRepository, tokenRepository };
