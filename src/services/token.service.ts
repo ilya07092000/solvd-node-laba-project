354efd2208c6273806b802e08377e750875d1f4d
ilya07092000 <ischenko2012@gmail.com>
2023-10-17 15:47:13 +0300

    feat: token service

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -1,21 +1,24 @@
 import { getFutureTimestampByMinutes } from '@src/helpers/timestamps';
 import HttpException from '@src/infrastructure/exceptions/httpException';
 import IToken from '@src/infrastructure/interfaces/IToken';
-import tokenRepository from '@src/repositories/tokenRepository';
+import {
+  TokenRepository,
+  tokenRepository,
+} from '@src/repositories/tokenRepository';
 
 class TokenService {
-  private repository;
+  private repository: TokenRepository;
 
-  constructor(repository) {
+  constructor(repository: TokenRepository) {
     this.repository = repository;
   }
 
-  private getToken(): number {
+  private getToken(): string {
     return (
       Date.now() -
       Math.floor(Math.random() * 100) +
       Math.floor(Math.random() * 200)
-    );
+    ).toString();
   }
 
   public getAccessToken(): IToken {
@@ -23,6 +26,7 @@ class TokenService {
       +process.env.ACCESS_TOKEN_EXPIRATION_MINUTES,
     );
     return {
+      type: 'access',
       token: this.getToken(),
       expirationTimeStamp,
     };
@@ -33,6 +37,7 @@ class TokenService {
       +process.env.REFRESH_TOKEN_EXPIRATION_MINUTES,
     );
     return {
+      type: 'refresh',
       token: this.getToken(),
       expirationTimeStamp,
     };
@@ -46,11 +51,33 @@ class TokenService {
   }
 
   public saveToken({ userId, token }: { userId: number; token: IToken }) {
-    return this.repository.save({
-      token: token.token,
-      tokenInfo: { expirationTimeStamp: token.expirationTimeStamp, userId },
-    });
+    return this.repository.save({ token, userId });
   }
+
+  public async validateRefreshToken({ refreshToken }): Promise<{
+    token: IToken & { userId: number };
+    isValid: boolean;
+  }> {
+    const token = await this.repository.get({ token: refreshToken });
+    return {
+      token,
+      isValid:
+        +token.expirationTimeStamp > +Date.now() || token.type === 'refresh',
+    };
+  }
+
+  // public async refreshTokens({ refreshToken }) {
+  //   const token = await this.repository.get({ token: refreshToken });
+
+  //   if (+token.expirationTimeStamp < +Date.now() || token.type !== 'refresh') {
+  //     throw new HttpException(400, 'Token is not valid');
+  //   }
+  //   const tokens = this.generateTokens();
+  //   await this.saveToken({ userId: token.userId, token: tokens.refreshToken });
+  //   await this.saveToken({ userId: token.userId, token: tokens.accessToken });
+
+  //   return tokens;
+  // }
 }
 
 const tokenService = new TokenService(tokenRepository);
