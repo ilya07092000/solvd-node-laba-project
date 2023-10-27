de2124334051532221e44a256514aa2accafce49
ilya07092000 <ischenko2012@gmail.com>
2023-10-27 14:48:01 +0300

    added private paths

--- src/router/admins.router.ts
+++ src/router/admins.router.ts
@@ -2,7 +2,10 @@ import { Router } from 'express';
 const router = Router();
 
 import adminsController from '@src/controllers/admins.controller';
+import authMiddleware from '@src/middlewares/auth.middleware';
+import RoleTypes from '@src/infrastructure/enums/roles';
 
+router.use(authMiddleware([RoleTypes.ADMIN]));
 router.get('/', adminsController.getAll);
 router.get('/:id', adminsController.getById);
 router.post('/', adminsController.create);
--- src/router/roles.router.ts
+++ src/router/roles.router.ts
@@ -2,9 +2,16 @@ import { Router } from 'express';
 const router = Router();
 
 import rolesController from '@src/controllers/roles.controller';
+import authMiddleware from '@src/middlewares/auth.middleware';
+import RoleTypes from '@src/infrastructure/enums/roles';
 
 router.get('/', rolesController.getAll);
 router.get('/:id', rolesController.getById);
+
+/**
+ * PRIVATE
+ */
+router.use(authMiddleware([RoleTypes.ADMIN]));
 router.post('/', rolesController.create);
 router.put('/:id', rolesController.update);
 router.delete('/:id', rolesController.deleteById);
